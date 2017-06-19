ae400780b6caa4078ced968e06e5a25620598e5d
Nic Mitchell <nic@nicmitchell.com>
2017-06-18 21:08:35 -0700

    changed default view to Wallet

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     this.state = {
       accounts: [],
       currentSelectedPayment: {},
-      actionState: 'manageWallet',
+      actionState: 'wallet',
     };
   }
 
